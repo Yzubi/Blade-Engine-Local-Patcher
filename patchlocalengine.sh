bdc7ec642acc781ad4362a57fcac8f9b48ab8def
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-23 02:43:36 -0200

    Update patchlocalengine.sh

--- patchlocalengine.sh
+++ patchlocalengine.sh
@@ -1,7 +1,7 @@
 cd "${0%/*}"
 options="$@"
 cd "$options"
-mkdir libs
+#mkdir libs
 if [ "$(cat core/build.gradle | grep 'api "com.bladecoder.engine:blade-engine:$bladeEngineVersion"')" != "" ]; then
 sed -i 's/api "com.bladecoder.engine:blade-engine:$bladeEngineVersion"//g' core/build.gradle
 echo Patch complete!
