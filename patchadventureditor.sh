a81e4a3495716aab7711e26c13daa7e0e294d5fe
Yzubi <42348748+Yzubi@users.noreply.github.com>
2018-12-23 02:34:44 -0200

    Update patchadventureditor.sh

--- patchadventureditor.sh
+++ patchadventureditor.sh
@@ -1,7 +1,7 @@
 cd "${0%/*}"
 options="$@"
 cd "$options"
-mkdir -p /blade-engine/build
+mkdir -p blade-engine/build
 if [ "$(cat adventure-editor/src/main/resources/projectTmpl/core/build.gradle | grep 'api "com.bladecoder.engine:blade-engine:$bladeEngineVersion"')" != "" ]; then
 sed -i 's/api "com.bladecoder.engine:blade-engine:$bladeEngineVersion"//g' adventure-editor/src/main/resources/projectTmpl/core/build.gradle
 echo Adventure Editor patched!
